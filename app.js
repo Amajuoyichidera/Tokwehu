7faf753c6b5503d69e28bb365c3d00befd88c21b
Amajuoyi chidera <Amajuoyichideradavid@gmail.com>
2022-12-19 12:24:23 -0800

    first version

---
+++ app.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    setTimeout(()=>{
+      $('#Preloder').addClass('loaded');
+    },1500);
+    setTimeout(()=>{
+      $('#Preloder').remove('loaded');
+    },4000);
+  })
+
+
+  const body = document.querySelector("body");
+    const navbar = document.querySelector(".navbar");
+    const menuBtn = document.querySelector(".menu-btn");
+    const cancelBtn = document.querySelector(".cancel-btn");
+    menuBtn.onclick = ()=>{
+      navbar.classList.add("show");
+      menuBtn.classList.add("hide");
+      body.classList.add("disabled");
+    }
+    cancelBtn.onclick = ()=>{
+      body.classList.remove("disabled");
+      navbar.classList.remove("show");
+      menuBtn.classList.remove("hide");
+    }
+    window.onscroll = ()=>{
+      this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+    }
