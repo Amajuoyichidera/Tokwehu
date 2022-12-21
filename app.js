3262d594fd3cac23c878b27b888dc92aa3c9b8bd
Amajuoyi chidera <Amajuoyichideradavid@gmail.com>
2022-12-21 11:52:48 -0800

    first version

--- app.js
+++ app.js
@@ -43,4 +43,19 @@ $(document).ready(function() {
       $('html, body').animate({scrollTop:0}, '300');
     });
     
+
     
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
