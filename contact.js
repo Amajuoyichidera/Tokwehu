565a4de15c73308b8f84399c8e5f546c19f5f920
Amajuoyi chidera <Amajuoyichideradavid@gmail.com>
2022-12-23 14:39:39 -0800

    first version

---
+++ contact.js
@@ -0,0 +1,43 @@
+const body = document.querySelector("body");
+    const navbar = document.querySelector(".navbar");
+    const menuBtn = document.querySelector(".menu-btn");
+    const cancelBtn = document.querySelector(".cancel-btn");
+    menuBtn.onclick = ()=>{
+      navbar.classList.add("show");
+      menuBtn.classList.add("hide");
+      body.classList.add("disabled");
+    }
+    cancelBtn.onclick = ()=>{
+      body.classList.remove("disabled");
+      navbar.classList.remove("show");
+      menuBtn.classList.remove("hide");
+    }
+    window.onscroll = ()=>{
+      this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky");
+    }
+
+
+    $(document).ready(function() {
+      setTimeout(()=>{
+        $('#Preloder').addClass('loaded');
+      },1500);
+      setTimeout(()=>{
+        $('#Preloder').remove('loaded');
+      },4000);
+    })
+
+
+    var btn = $('#button');
+
+    $(window).scroll(function() {
+      if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+      } else {
+        btn.removeClass('show');
+      }
+    });
+    
+    btn.on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({scrollTop:0}, '300');
+    });
